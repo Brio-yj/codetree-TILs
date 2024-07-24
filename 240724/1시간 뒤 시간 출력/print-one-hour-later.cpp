02bc73e01fd0be9bef426d0e5d9dc9d5987a098c
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-24 14:26:58 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a;
+	cin.get();
+	cin >> b;
+    if(a==24)a=0;
+	cout << a+1 << endl << b;
+    return 0;
+}
