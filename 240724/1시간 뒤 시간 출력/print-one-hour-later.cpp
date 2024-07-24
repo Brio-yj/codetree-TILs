a449d3e2a2e0cbbcb920b6d26b4766558895b556
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-24 14:28:03 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/1시간 뒤 시간 출력/print-one-hour-later.cpp
+++ 240724/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -4,10 +4,11 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
+    char c;
     cin >> a;
-	cin.get();
+    cin>>c;
 	cin >> b;
     if(a==24)a=0;
-	cout << a+1 <<" "<< b;
+	cout << a+1 <<c<< b;
     return 0;
 }
