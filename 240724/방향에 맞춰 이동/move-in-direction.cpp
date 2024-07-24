4f3c417d3f3ea5990e767ac47e40598c46093385
조용진 <101401582+Brio-yj@users.noreply.github.com>
2024-07-24 15:06:22 +0900

    Title: 방향에 맞춰 이동, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240724/방향에 맞춰 이동/move-in-direction.cpp
+++ 240724/방향에 맞춰 이동/move-in-direction.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+
+int dx[4]={-1,1,0,0};
+int dy[4]={0,0,-1,1};
+
 int main() {
     // 여기에 코드를 작성해주세요.
+    int N;
+    cin>>N;
+    int x=0,y=0;
+    for(int i=0;i<N;i++){
+        char ch;
+        int num;
+        if(ch=="N"){
+            x+=dx[2];
+            y+=dy[2]*num;
+        }
+        if(ch=="E"){
+            x+=dx[1]*num;
+            y+=dy[1];
+        }if(ch=="S"){
+            x+=dx[3];
+            y+=dy[3]*num;
+        }if(ch=="E"){
+            x+=dx[1]*num;
+            y+=dy[1];
+        }
+    }
+    cout<<x<<" "<<y;
     return 0;
 }
